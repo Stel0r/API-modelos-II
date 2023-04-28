ceeee86bb7d06b5e46abe4558a0b3adb4b751091
Stel0r <65517739+Stel0r@users.noreply.github.com>
2023-04-28 16:45:10 -0500

    agregacion de destino cross origin agregada

---
+++ SQL/generar-datos.sql
@@ -0,0 +1,56 @@
+INSERT INTO public."Rol"("nombreRol")
+	VALUES ('Tecnico');
+INSERT INTO public."Rol"("nombreRol")
+	VALUES ('CliNOVIP');
+INSERT INTO public."Rol"("nombreRol")
+	VALUES ('CliTest');
+INSERT INTO public."Rol"("nombreRol")
+	VALUES ('Admin');
+INSERT INTO public."Rol"("nombreRol")
+	VALUES ('CliVIP');
+
+
+
+
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (1, 5);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (1, 1);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (2, 3);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (2, 6);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (3, 5);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (3, 1);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (3, 2);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (3, 3);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (3, 4);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (4, 5);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (4, 1);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (4, 2);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (5, 3);
+INSERT INTO public."Rol_Permiso"(
+	"Rol_idRol", "Permiso_idPermiso")
+	VALUES (5, 4);
